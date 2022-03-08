b346993d943f08615a144bd00dff6b10c558b8d1
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-03-08 20:54:15 +0200

    Create Customers List Relates #15

--- server/router/route.js
+++ server/router/route.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const {getPackagesHandler} = require('../controllers/getPackages')
-const {postStoreHandler} = require('../controllers/addStore')
+const { getPackagesHandler } = require('../controllers/getPackages');
+const { postStoreHandler } = require('../controllers/addStore');
+const { getCustomersHandler } = require('../controllers/postPackges');
 
 const router = express.Router();
 
 router.get('/', getPackagesHandler);
 
 router.post('/add-store', postStoreHandler);
+
+router.get('/get-customers', getCustomersHandler);
 
 module.exports = router;
