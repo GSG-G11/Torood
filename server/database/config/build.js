c282920839f517be8f6ed04a39da2d1b9ebc9f13
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-03-08 18:06:36 +0200

    DataBase Server

---
+++ server/database/config/build.js
@@ -0,0 +1,11 @@
+const { join } = require('path');
+const { readFileSync } = require('fs');
+
+const connetion = require('./connection');
+
+const dbBuild = () => {
+  const sql = readFileSync(join(__dirname, 'build.sql')).toString();
+  return connetion.query(sql);
+};
+
+module.exports = { dbBuild };
--- server/router/route.js
+++ server/router/route.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const getPackages = require('../database/queries/getPackages');
 const router = express.Router();
 
-router.get('/');
+router.get('/', (req, res) => {
+  getPackages().then((data) => res.json(data.rows));
+});
 
 module.exports = router;
