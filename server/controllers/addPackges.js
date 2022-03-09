b8fa401e901c0024be738c5c84d03f0f511a8d11
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-03-09 12:44:14 +0200

    Forms Validation Relates #30

--- server/controllers/addPackges.js
+++ server/controllers/addPackges.js
@@ -1,10 +1,11 @@
 const { addPackage } = require('../database/queries/postPackages');
-const {join} = require('path')
+const { join } = require('path');
+
 const addPackageHandler = (req, res) => {
   const { name, customer_name, store, image, date, price } = req.body;
-  addPackage(name, customer_name, store, image, date, price)
-    // .then((data) => res.json(data.rows))
-    .then(res.sendFile(join(__dirname,'..','..' , 'client','main.html')));
+  addPackage(name, customer_name, store, image, date, price).then(
+    res.sendFile(join(__dirname, '..', '..', 'client', 'main.html'))
+  );
 };
 
 module.exports = { addPackageHandler };
